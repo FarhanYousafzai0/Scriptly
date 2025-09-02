d576e4d9c98d1cd30dd8b7565cc8cfc682121fec
Farhan Yousafzai <coderwithferry@gmail.com>
2025-09-03 01:53:18 +0500

    Scriptly

---
+++ app/(root)/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=' flex items-center justify-center text-4xl bg-red-500 h-screen w-screen font-bold'>page</div>
+  )
+}
+
+export default page
